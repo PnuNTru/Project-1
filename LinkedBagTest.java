3b226990f6b34eb2e0600ed5622ae916f5c029ea
Phu Truong <phutrustuff@gmail.com>
2022-02-23 00:53:50 -0800

    added more entries to be compared

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -4,6 +4,7 @@ public class LinkedBagTest
     
     public static void main(String[] args)
     {
+        
     
         BagInterface<String> bag1 = new LinkedBag<>();
         BagInterface<String> bag2 = new LinkedBag<>();
@@ -13,17 +14,19 @@ public class LinkedBagTest
         bag1.add("C");
         bag1.add("D");
         bag1.add("E");
+        bag1.add("F");
         bag2.add("A");
         bag2.add("B");
         bag2.add("C");
         bag2.add("D");
         bag2.add("E");
+        bag2.add("G");
         System.out.println("The contents of bag1 is " + Arrays.toString(bag1.toArray()));
 
         System.out.println("The union of bag1 and bag2 is " + Arrays.toString(bag1.union(bag2).toArray()));
 
         System.out.println("The intersection of bag1 and bag2 is " + Arrays.toString(bag1.intersection(bag2).toArray()));
 
-        System.out.println("The difference of bag1 and bag2 is " + Arrays.toString(bag1.difference(bag2).toArray()));
+        System.out.println("The difference between bag1 and bag2 is " + Arrays.toString(bag1.difference(bag2).toArray()));
     }
 }
