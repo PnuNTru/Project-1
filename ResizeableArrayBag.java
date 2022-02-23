a009303c6c6b55fe477122bd365662bdc61c3832
Phu Truong <phutrustuff@gmail.com>
2022-02-23 01:34:27 -0800

    added private ints, added revised add using Array Resizing

--- ResizeableArrayBag.java
+++ ResizeableArrayBag.java
@@ -1,9 +1,53 @@
-//Project 1
-//CS2400.03
-//Febraury 28, 2022
-//Phu Troung, Sarah Camacho, Jonathan Pena
-
-public class LinkedBag<T> implements BagInterface<T>
+import java.util.Arrays;
+public class ResizeableArrayBag<T> implements BagInterface<T>
 {
+    private T[] bag;
+    private static final int DEFAULT_CAPACITY = 25;
+    private int numberOfEntries;
+    public ResizeableArrayBag(int capacity)
+    {
+        
+
+    }
+    
+    public ResizeableArrayBag()
+    {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /** Adds a new entry to this bag.
+        @param newEntry The object to obe added as a new entry.
+        @return True. */
+    public boolean add(T newEntry)
+    {
+        checkIntegrity();
+        boolean result = true;
+        if (isArrayFull())
+        {
+            doubleCapacity();
+        } // end if
+
+        bag[numberOfEntries] = newEntry;
+        numberOfEntries++;
+
+        return true;
+    } // end add
+
+    // Doubles the size of the array bag.
+    // Precondition: checkIntegrity has been called.
+    private void doubleCapacity()
+    {
+        int newLength = 2 * bag.length;
+        checkCapacity(newLength);
+        bag = Arrays.copyOf(bag, newLength);
+    } // end doubleCapacity
+
+    // Throws an exception if the client requests a capacity that is too large.
+    private void checkCapacity(int capacity)
+    {
+        if (capacity > MAX_CAPACITY)
+         throw new IllegalStateException("Attempt to create a bag whose " + "capacity exceeds allowed " + "maximum of " + MAX_CAPACITY);
+    } // end checkCapacity
+
 
 }
