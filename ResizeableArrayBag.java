1d88b3560924775710e500eba65c73a28bf37102
Phu Truong <phutrustuff@gmail.com>
2022-02-23 00:46:27 -0800

    added more entries to be compared

--- ResizeableArrayBag.java
+++ ResizeableArrayBag.java
@@ -1,9 +1,32 @@
-//Project 1
-//CS2400.03
-//Febraury 28, 2022
-//Phu Troung, Sarah Camacho, Jonathan Pena
-
-public class LinkedBag<T> implements BagInterface<T>
+import java.util.Arrays;
+public class ResizeableArrayBag
 {
+    
+    public static void main(String[] args)
+    {
+        
+    
+        BagInterface<String> bag1 = new ResizeableArrayBag<>();
+        BagInterface<String> bag2 = new ResizeableArrayBag<>();
 
+        bag1.add("A");
+        bag1.add("B");
+        bag1.add("C");
+        bag1.add("D");
+        bag1.add("E");
+        bag1.add("F");
+        bag2.add("A");
+        bag2.add("B");
+        bag2.add("C");
+        bag2.add("D");
+        bag2.add("E");
+        bag2.add("G");
+        System.out.println("The contents of bag1 is " + Arrays.toString(bag1.toArray()));
+
+        System.out.println("The union of bag1 and bag2 is " + Arrays.toString(bag1.union(bag2).toArray()));
+
+        System.out.println("The intersection of bag1 and bag2 is " + Arrays.toString(bag1.intersection(bag2).toArray()));
+
+        System.out.println("The difference between bag1 and bag2 is " + Arrays.toString(bag1.difference(bag2).toArray()));
+    }
 }
