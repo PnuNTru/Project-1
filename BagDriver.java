9e2be61c7fca7080e05508ec8783ed068448ddc8
Jonathan Pena <98781952+JonathanPena17@users.noreply.github.com>
2022-02-28 08:16:59 -0800

    Update BagDriver.java

--- BagDriver.java
+++ BagDriver.java
@@ -1,5 +1,66 @@
+//CS2400
+//Project 1
+//February 28, 2022
+//Phu Troung Sarah Camacho Jonathan Pena
+
+import java.util.Arrays;
+
 public class BagDriver
 {
+    public static void main(String [] args)
+    {
+        Baginterface<String> arrayBag1 = newResizeablearray<>();
+        arrayBag1.add();
+        arrayBag1.add();
+        arrayBag1.add();
+        arrayBag1.add();
+        arrayBag1.add();
+        arrayBag1.add();
+        arrayBag1.add();
+        
+        Baginterface<String> arrayBag2 = newResizeablearray<>();
+        arrayBag2.add();
+        arrayBag2.add();
+        arrayBag2.add();
+        arrayBag2.add();
+        arrayBag2.add();
+        arrayBag2.add();
+        arrayBag2.add();
+        
+        BagInterface<String> linkedBag1 = new LinkedBag<>();
+        linkedBag1.add();
+        linkedBag1.add();
+        linkedBag1.add();
+        linkedBag1.add();
+        linkedBag1.add();
+        linkedBag1.add();
+        linkedBag1.add();
+        
+        BagInterface<String> linkedBag2 = new LinkedBag<>();
+        linkedBag2.add();
+        linkedBag2.add();
+        linkedBag2.add();
+        linkedBag2.add();
+        linkedBag2.add();
+        linkedBag2.add();
+        linkedBag2.add();
+        
+        System.out.println("Union between two array bags: " + Arrays.toString(arrayBag1.union(arrayBag2).toArray()));
+        System.out.println("Intersection between two array bags: " + Arrays.toString(arrayBag1.intersection(arrayBag2).toArray()));
+        System.out.println("Difference between two array bags: " + Arrays.toString(arrayBag1.difference(arrayBag2).toArray()));
+        
+        System.out.println("Union between two linked bags: " + Arrays.toString(linkedBag1.union(linkedBag2).toArray()));
+        System.out.println("Intersection between two linked bags: " + Arrays.toString(linkedBag1.intersection(linkedBag2).toArray()));
+        System.out.println("Difference between two linked bags: " + Arrays.toString(linkedBag1.difference(linkedBag2).toArray()));
+        
+        
+        
+        
+    
+    
+    
+    
+    
     // array bag test
     
     //union demo
@@ -9,5 +70,4 @@ public class BagDriver
     
     //linked bag test
     
-    //
 }
