6a27fd0f7195ff8fb84671e2dacd1bbd4588dd51
Jonathan Pena <98781952+JonathanPena17@users.noreply.github.com>
2022-02-28 08:35:11 -0800

    Update Bagdriver.java

--- BagDriver.java
+++ BagDriver.java
@@ -9,65 +9,47 @@ public class BagDriver
 {
     public static void main(String [] args)
     {
-        BagInterface<String> arrayBag1 = new ResizeableArrayBag<>();
-        arrayBag1.add();
-        arrayBag1.add();
-        arrayBag1.add();
-        arrayBag1.add();
-        arrayBag1.add();
-        arrayBag1.add();
-        arrayBag1.add();
+        Baginterface<String> arrayBag1 = newResizeablearray<>();
+            arrayBag1.add("A");
+            arrayBag1.add("B");
+            arrayBag1.add("C");
+            arrayBag1.add("D");
+            arrayBag1.add("E");
+            arrayBag1.add("F");
         
-        BagInterface<String> arrayBag2 = new ResizeableArrayBag<>();
-        arrayBag2.add();
-        arrayBag2.add();
-        arrayBag2.add();
-        arrayBag2.add();
-        arrayBag2.add();
-        arrayBag2.add();
-        arrayBag2.add();
+        Baginterface<String> arrayBag2 = newResizeablearray<>();
+        
+            arrayBag2.add("A");
+            arrayBag2.add("Z");
+            arrayBag2.add("H");
+            arrayBag2.add("B");
+            arrayBag2.add("C");
+            arrayBag2.add("E");
+            arrayBag2.add("F");
         
         BagInterface<String> linkedBag1 = new LinkedBag<>();
-        linkedBag1.add();
-        linkedBag1.add();
-        linkedBag1.add();
-        linkedBag1.add();
-        linkedBag1.add();
-        linkedBag1.add();
-        linkedBag1.add();
+        
+            linkedBag1.add("A");
+            linkedBag1.add("X");
+            linkedBag1.add("Y");
+            linkedBag1.add("Z");
+            linkedBag1.add("B");
+            linkedBag1.add("C");
         
         BagInterface<String> linkedBag2 = new LinkedBag<>();
-        linkedBag2.add();
-        linkedBag2.add();
-        linkedBag2.add();
-        linkedBag2.add();
-        linkedBag2.add();
-        linkedBag2.add();
-        linkedBag2.add();
+            
+            linkedBag2.add("B");
+            linkedBag2.add("C");
+            linkedBag2.add("H");
+            linkedBag2.add("J:);
+            linkedBag2.add("S");
+            linkedBag2.add("Y");
         
-        System.out.println("Union between two array bags: " + Arrays.toString(arrayBag1.union(arrayBag2).toArray()));
-        System.out.println("Intersection between two array bags: " + Arrays.toString(arrayBag1.intersection(arrayBag2).toArray()));
-        System.out.println("Difference between two array bags: " + Arrays.toString(arrayBag1.difference(arrayBag2).toArray()));
+        System.out.println("The Union between arrayBag1 & arrayBay2 is: " + Arrays.toString(arrayBag1.union(arrayBag2).toArray()));
+        System.out.println("The Intersection between arrayBag1 & arrayBay2 is: " + Arrays.toString(arrayBag1.intersection(arrayBag2).toArray()));
+        System.out.println("The Difference between arrayBag1 & arrayBay2 is: " + Arrays.toString(arrayBag1.difference(arrayBag2).toArray()));
         
-        System.out.println("Union between two linked bags: " + Arrays.toString(linkedBag1.union(linkedBag2).toArray()));
-        System.out.println("Intersection between two linked bags: " + Arrays.toString(linkedBag1.intersection(linkedBag2).toArray()));
-        System.out.println("Difference between two linked bags: " + Arrays.toString(linkedBag1.difference(linkedBag2).toArray()));
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    // array bag test
-    
-    //union demo
-    //intersection demo
-    //difference demo
-    
-    
-    //linked bag test
-    
+        System.out.println("The Union between linkedBag1 & linkedBag2 is: " + Arrays.toString(linkedBag1.union(linkedBag2).toArray()));
+        System.out.println("The Intersection between linkedBag1 & linkedBag2 is: " + Arrays.toString(linkedBag1.intersection(linkedBag2).toArray()));
+        System.out.println("The Difference between linkedBag1 & linkedBag2 is: " + Arrays.toString(linkedBag1.difference(linkedBag2).toArray()));
 }
