6212e779727e93daaf10f651ba0e8395a9efce47
sacamacho <100239293+sacamacho@users.noreply.github.com>
2022-02-28 10:51:50 -0800

    Update ResizeableArrayBag.java

--- ResizeableArrayBag.java
+++ ResizeableArrayBag.java
@@ -5,35 +5,67 @@ public class ResizeableArrayBag<T> implements BagInterface<T>
     private static final int DEFAULT_CAPACITY = 25;
     private int numberOfEntries;
     
-    private boolean checkIntegrity = false;
+    private boolean integrity = false;
     private static final int MAX_CAPACITY = 100;
-    public ResizeableArrayBag(int capacity)
-    {
-
-    }
-    
-    public ResizeableArrayBag()
+    public ResizableArrayBag()
     {
         this(DEFAULT_CAPACITY);
     }
-
+    public ResizeableArrayBag(int capacity)
+    {
+        if (capacity <= MAX_CAPACITY)
+        {
+            @SupressWarnings("uncheckeed")
+            T[] tempBag = (T[])new Object[capacity];
+            bag = tempBag;
+            numberOfEntries = 0;
+            integrity = true;
+        }
+        else
+            throw new IllegalStateException("Attempt to create a bag whose capacity exceeds allowed " + "maximum of " + MAX_CAPACITY);
+    }
     /** Adds a new entry to this bag.
-        @param newEntry The object to obe added as a new entry.
+        @param newEntry The object to be added as a new entry.
         @return True. */
     public boolean add(T newEntry)
     {
-        checkIntegrity= true;
-        if (numberOfEntries == DEFAULT_CAPACITY)
+        boolean result = true;
+        if (isFull())
         {
-            doubleCapacity();
-        } // end if
+            result = false
+        }
+        else
+        {
+            bag[numberOfEntries = newEntry;
+            numberOfEntries++;
+        }
 
         bag[numberOfEntries] = newEntry;
         numberOfEntries++;
 
-        return checkIntegrity;
+        return result;
     } // end add
 
+    public T[] toArray()
+    {
+        @SuppressWarnings("unchecked")
+        T[] result = (T[])new Object[numberOfEntries];
+        for (int index = 0; index < numberOfEntries; index++)
+        {
+            result[index] = bag[index];
+        }
+        return result;
+    }
+    
+    public boolean isEmpty()
+    {
+        return numberOfEntries == 0;
+    } // end isEmpty
+    
+    public boolean isFull()
+    {
+        return numberOfEntries == bag.length;
+    }
     // Doubles the size of the array bag.
     // Precondition: checkIntegrity has been called.
     private void doubleCapacity()
@@ -42,13 +74,9 @@ public class ResizeableArrayBag<T> implements BagInterface<T>
         checkCapacity(newLength);
         bag = Arrays.copyOf(bag, newLength);
     } // end doubleCapacity
-
-    // Throws an exception if the client requests a capacity that is too large.
-    private void checkCapacity(int capacity)
+    private void checkIntegrity()
     {
-        if (capacity > MAX_CAPACITY)
-         throw new IllegalStateException("Attempt to create a bag whose " + "capacity exceeds allowed " + "maximum of " + MAX_CAPACITY);
-    } // end checkCapacity
-
-
+     if (!integrity)
+        throw new SecurityException("ResizableArrayBag is corrupt.");
+    }
 }
