7e151d432cea06050ee40dafddd6e09908214d83
sacamacho <100239293+sacamacho@users.noreply.github.com>
2022-02-28 13:58:12 -0800

    Update LinkedBag.java

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,7 +1,7 @@
 //Project 1
 //CS2400.03
 //Febraury 28, 2022
-//Phu Troung, Sarah Camacho, Jonathan Pena
+//Phu Truong, Sarah Camacho, Jonathan Pena
 
 public class LinkedBag<T> implements BagInterface<T>
 {
